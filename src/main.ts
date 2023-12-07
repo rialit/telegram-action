2d0bca013863ccc4cee8c7796fb6bbc4315db7cf
sivanov <rialit1993@gmail.com>
2023-12-07 15:48:07 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -40,11 +40,13 @@ async function main() {
         const to = core.getInput('to');
         const token = core.getInput('token');
         const commits = github.context.payload.commits.filter((commit: Commit) => commit.distinct && isUpdateVersion(commit.message));
+
+        console.log('test')
         
         if(commits.length < 1) {
             return;
         }
-        
+
         const packageJson = getPackage();
         
         const telegramMessageArray = [

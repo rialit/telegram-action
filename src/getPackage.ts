4d0c22657dc0d0bd81048e888875569c7765f2bf
sivanov <rialit1993@gmail.com>
2023-12-07 13:17:34 +0700

    edit

--- src/getPackage.ts
+++ src/getPackage.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
-import path = require("path");
 import * as core from '@actions/core';
+import path = require("path");
 
 export interface PackageJson {
     name: string,
@@ -8,21 +8,7 @@ export interface PackageJson {
 }
 
 export default function(): PackageJson {
-
-    const corePath = core.getInput('path');
-
-    console.log(corePath)
-
-    console.log(fs.readFileSync(path.join(corePath, 'package.json')).toString())
-
-    let patchBack = '../';
-    for (let index = 0; index < 10; index++) {
-        console.log('index - ' + index);
-        console.log(fs.readdirSync(path.join(__dirname, patchBack)));
-        patchBack += '../'
-    }
-
-    const pathPackage = path.join(__dirname, '../../../../package.json');
+    const pathPackage = path.join(core.getInput('path'), 'package.json');
     const packageProject: PackageJson = {
         name: '',
         version: '',
